b88e877b176ab8868421395338762a69b6547bed
Your Name <you@example.com>
2017-04-06 15:02:57 +0200

    Add route config

---
+++ Config/routes.php
@@ -0,0 +1,11 @@
+<?php
+  $routes = array(
+    '' => array(
+      'controller' => 'index',
+      'action' => 'index'
+    ),
+    'hello' => array(
+      'controller' => 'index',
+      'action' => 'hello_world'
+    )
+  );
