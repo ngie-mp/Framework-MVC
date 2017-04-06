8fc6d3f61153eab79c9980f21ea9108463e55003
Your Name <you@example.com>
2017-04-06 16:26:49 +0200

    Manage 404 error page

--- Controllers/IndexController.php
+++ Controllers/IndexController.php
@@ -1,11 +1,12 @@
 <?php
   class IndexController extends Controller
   {
-      public function index(){
-        $this->render('index', array('PageName' => "Home page"));
+      public function index()
+      {
+          $this->render('index', array('PageName' => "Home page"));
       }
 
-      public function contact(){
-        $this->render('contact', array('PageName' => "Contact page"));
+      public function error404(){
+          $this->render('error404');
       }
   }
